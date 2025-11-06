e05fab01925bf13ecb23b93e5fd9602faa6df866
yordan <yordanmarin@gmail.com>
2025-11-06 13:27:32 -0500

    sem12

---
+++ GrafoAdyacencia/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace GrafoAdyacencia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textProfundidad = new System.Windows.Forms.TextBox();
+            this.textAmplitud = new System.Windows.Forms.TextBox();
+            this.btnProfundidad = new System.Windows.Forms.Button();
+            this.btnAmplitud = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnMatriz = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textDestino = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.textOrigen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textProfundidad
+            // 
+            this.textProfundidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textProfundidad.Location = new System.Drawing.Point(400, 490);
+            this.textProfundidad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textProfundidad.Name = "textProfundidad";
+            this.textProfundidad.Size = new System.Drawing.Size(444, 30);
+            this.textProfundidad.TabIndex = 55;
+            // 
+            // textAmplitud
+            // 
+            this.textAmplitud.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAmplitud.Location = new System.Drawing.Point(400, 438);
+            this.textAmplitud.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textAmplitud.Name = "textAmplitud";
+            this.textAmplitud.Size = new System.Drawing.Size(444, 30);
+            this.textAmplitud.TabIndex = 54;
+            // 
+            // btnProfundidad
+            // 
+            this.btnProfundidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProfundidad.Location = new System.Drawing.Point(248, 483);
+            this.btnProfundidad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnProfundidad.Name = "btnProfundidad";
+            this.btnProfundidad.Size = new System.Drawing.Size(147, 47);
+            this.btnProfundidad.TabIndex = 53;
+            this.btnProfundidad.Text = "Profundidad";
+            this.btnProfundidad.UseVisualStyleBackColor = true;
+            this.btnProfundidad.Click += new System.EventHandler(this.btnProfundidad_Click);
+            // 
+            // btnAmplitud
+            // 
+            this.btnAmplitud.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAmplitud.Location = new System.Drawing.Point(248, 430);
+            this.btnAmplitud.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAmplitud.Name = "btnAmplitud";
+            this.btnAmplitud.Size = new System.Drawing.Size(147, 47);
+            this.btnAmplitud.TabIndex = 52;
+            this.btnAmplitud.Text = "Amplitud";
+            this.btnAmplitud.UseVisualStyleBackColor = true;
+            this.btnAmplitud.Click += new System.EventHandler(this.btnAmplitud_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(248, 164);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(596, 242);
+            this.richTextBox1.TabIndex = 51;
+            this.richTextBox1.Text = "";
+            // 
+            // btnMatriz
+            // 
+            this.btnMatriz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMatriz.Location = new System.Drawing.Point(248, 90);
+            this.btnMatriz.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMatriz.Name = "btnMatriz";
+            this.btnMatriz.Size = new System.Drawing.Size(597, 54);
+            this.btnMatriz.TabIndex = 50;
+            this.btnMatriz.Text = "MATRIZ DE ADYACENCIA";
+            this.btnMatriz.UseVisualStyleBackColor = true;
+            this.btnMatriz.Click += new System.EventHandler(this.btnMatriz_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 38);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "GRAFOS DIRIGIDOS";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(49, 255);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 254);
+            this.listBox1.TabIndex = 61;
+            // 
+            // textDestino
+            // 
+            this.textDestino.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDestino.Location = new System.Drawing.Point(134, 128);
+            this.textDestino.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textDestino.Name = "textDestino";
+            this.textDestino.Size = new System.Drawing.Size(81, 30);
+            this.textDestino.TabIndex = 60;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Destino:";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.Location = new System.Drawing.Point(88, 180);
+            this.btnInsertar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(129, 47);
+            this.btnInsertar.TabIndex = 58;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            // 
+            // textOrigen
+            // 
+            this.textOrigen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOrigen.Location = new System.Drawing.Point(134, 91);
+            this.textOrigen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textOrigen.Name = "textOrigen";
+            this.textOrigen.Size = new System.Drawing.Size(81, 30);
+            this.textOrigen.TabIndex = 57;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 25);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "Origen:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 571);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textDestino);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.textOrigen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textProfundidad);
+            this.Controls.Add(this.textAmplitud);
+            this.Controls.Add(this.btnProfundidad);
+            this.Controls.Add(this.btnAmplitud);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnMatriz);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textProfundidad;
+        private System.Windows.Forms.TextBox textAmplitud;
+        private System.Windows.Forms.Button btnProfundidad;
+        private System.Windows.Forms.Button btnAmplitud;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnMatriz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textDestino;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.TextBox textOrigen;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GrafoAdyacencia/Grafo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafoAdyacencia
+{
+    internal class Grafo
+    {
+        int[,] matriz;
+        int vertices;
+
+        public Grafo(int vertices)
+        {
+            this.vertices = vertices;
+            matriz = new int[vertices,vertices];
+        }
+
+        public void insertarArista(int o, int d)
+        {
+            matriz[o,d] = 1;
+            //matriz[d,o] = 1; si el grafo es no dirigido
+        }
+        public void imprimirArista(ListBox list, int o, int d)
+        {
+            list.Items.Add($"{o} | {d}");
+        }
+
+        public void imprimirMatriz(RichTextBox rich)
+        {
+            for(int i = 0;i < vertices; i++)
+            {
+                for (int j = 0;j < vertices; j++)
+                {
+                    rich.Text += matriz[i, j]+"\t";
+                }
+                rich.Text += "\n";
+            }
+        }
+
+        public void amplitud(TextBox text)
+        {
+            int inicio = 0;
+            bool[] visitado = new bool[vertices];
+            Queue<int> cola = new Queue<int>();
+
+            visitado[inicio] = true;
+            cola.Enqueue(inicio);
+
+            while(cola.Count() != 0)
+            {
+                int actual = cola.Dequeue();
+                text.Text += actual + " ";
+
+                for(int i = inicio;i < vertices; i++)
+                {
+                    if (matriz[actual,i] ==1 && !visitado[i])
+                    {
+                        visitado[i] = true;
+                        cola.Enqueue(i);
+                    }
+                }
+            }
+        }
+
+        public void profundidad(TextBox text)
+        {
+            int inicio = 0;
+            bool[] visitado = new bool[vertices];
+            Stack<int> pila = new Stack<int>();
+
+            visitado[inicio] = true;
+            pila.Push(inicio);
+
+            while (pila.Count() != 0)
+            {
+                int actual = pila.Pop();
+                text.Text += actual + " ";
+
+                for (int i = vertices-1; i >=inicio; i--)
+                {
+                    if (matriz[actual, i] == 1 && !visitado[i])
+                    {
+                        visitado[i] = true;
+                        pila.Push(i);
+                    }
+                }
+            }
+        }
+    }
+}
